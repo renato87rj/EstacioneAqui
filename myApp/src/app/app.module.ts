5ab73e3e4136f74b500544731eb82e1b6a15ec9c
renato <renato87rj@gmail.com>
2018-10-01 01:40:30 -0300

    atualizacao

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import {RegisterPage} from "../pages/register/register";
+import {LoginPage} from "../pages/login/login";
+import {PrincipalPage} from "../pages/principal/principal";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +16,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage, 
+    RegisterPage,
+    LoginPage,
+    PrincipalPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +29,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    RegisterPage,
+    LoginPage,
+    PrincipalPage
   ],
   providers: [
     StatusBar,
